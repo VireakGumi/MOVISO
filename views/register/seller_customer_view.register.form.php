3a8b2a5255189e0efcb82a8c561d2201504474c7
nit123456 <nit.noem.@student.passerellesnumeriques.org>
2023-01-23 19:12:43 +0700

    [MOS-53] create an interface web page form for choose customer and seller

---
+++ views/register/seller_customer_view.register.form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seller or Customer</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <img src="../../assets/background_seller_or_customer_register.jpg" class="background_seller_customer_choosin">\
+        <form action="" >
+            <div class="form_left">
+                <img src="../../assets/moviso-logo1.png" alt="" class="logo_on_form" width="65%">
+                <div class="information">
+                    <h2>Welcome</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum nemo explicabo quaerat aperiam numquam!</p>
+                </div>
+                <div class="find_at">
+                    <img src="../../assets/google_PNG102344.png" alt="" width="10%">
+                    <img src="../../assets/twitter.png" alt="" width="10%">
+                    <img src="../../assets/Telegram-Logo-PNG-Photo.png" alt="" width="10%">
+                </div>
+            </div>
+            <div class="form_right">
+                <h1>REGISTER</h1>
+                <p>Choose between customer and seller</p>
+                <div class="choosing">
+                    <div class="customer_or_seller">
+                        <input type="radio" value="Customer" name="choose">
+                        <label for="">Customer</label>
+                    </div>
+                    <div class="customer_or_seller">
+                        <input type="radio" value="Seller" name="choose">
+                        <label for="">Seller</label>
+                    </div>
+                </div>
+                <div class="btn">
+                    <button>Back</button>
+                    <button>Next</button>
+                </div>
+            </div>
+        </form>
+</body>
+</html>
