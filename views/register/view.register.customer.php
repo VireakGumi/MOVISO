ffa719451147c12ee70ad1b9cbaace6bf9136569
VireakGumi <chanvireak906@gmail.com>
2023-01-23 23:05:37 +0700

    [MOS-11] fix the class in css file

--- views/register/view.register.customer.php
+++ views/register/view.register.customer.php
@@ -11,9 +11,10 @@
 </head>
 
 <body>
+    <img src="../../assets/bg-register-customer.png" alt="" class="background">
     <form action="#" method="post" >
-        <div class="form-left">
-            <img src="../../assets/moviso-logo1.png" alt="" width="40%">
+        <div class="form_left">
+            <img src="../../assets/moviso-logo1.png" class="logo_on_form" alt="" width="65%">
             <h3>WELCOME</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.Velit aut facilis autem odio 
                 exercitationem Lorem ipsum dolor sit amet consectetur, adipisicing elit consectetur, adipisicing elit</p>
@@ -23,23 +24,23 @@
                 <img src="../../assets/icon-twifter.png" alt="" width="12%">
             </div>
         </div>
-        <div class="form-right">
+        <div class="form_right">
             <div class="container">
                 <h2>REGISTER</h2>
-                <input type="text" class="form-control" placeholder="User Name *    " name="Username" >
-                <small><?php echo $userName_error; ?></small>
+                <input type="text" class="form-control" placeholder="User Name *" name="Username" >
+                <!-- <small><?php echo $userName_error; ?></small> -->
 
                 <input type="text" class="form-control" placeholder="Email *" name="email" >
-                <small><?php echo $email_error; ?></small>
+                <!-- <small><?php echo $email_error; ?></small> -->
 
                 <input type="password" class="form-control" placeholder="Password *" name="Password">
-                <small><?php echo $password_error; ?></small>
+                <!-- <small><?php echo $password_error; ?></small> -->
 
                 <input type="date" class="form-control" placeholder="Date of birth *" name="dateofbirth">
-                <small><?php echo $date_error; ?></small>
+                <!-- <small><?php echo $date_error; ?></small> -->
 
                 <input type="number" class="form-control" placeholder="Credit Card *" name="creditcard">
-                <small><?php echo $phoneNumber_error; ?></small>
+                <!-- <small><?php echo $phoneNumber_error; ?></small> -->
 
                 <button>SUBMIT</button>
             </div>
