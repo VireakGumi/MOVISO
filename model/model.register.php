801124d287971d5c8599df40a0ea88b24f1fc26a
CHAMRETH.KOSAL <CHAMRETH.KOSAL@PNC.LAN>
2023-01-24 10:44:21 +0700

    [MOS-24] add model.register.php

--- model/model.register.php
+++ model/model.register.php
@@ -1,6 +1,18 @@
 <?php 
-    require'../database/database.php';
-    require'../controllers/validete/controller.validete.form.php';
 
-    
+function createCustomer($userName, $email,$password, $creditCard,$date)
+{
+    global $connection;
+    $statement = $connection->prepare("insert into customers (username,email,password,credit_card_number,date_of_birth) values (:username, :email, :password,:creditcard, :date)");
+    $statement->execute([
+       ':username' => $userName,    
+       ':email' => $email,
+       ':password' => $password,
+       ':date' => $date,
+       ':creditcard' => $creditCard,
+
+    ]);
+
+    return $statement->rowCount() > 0;
+}
 
