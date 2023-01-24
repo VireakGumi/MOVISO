3b0d9f9c58cf82e6f239472e284c6eca6c6c7c2e
NAVY.RORN <NAVY.RORN@PNC.LAN>
2023-01-24 14:46:24 +0700

    [MOS-30] login form

---
+++ views/login/view.login.form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> login form</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <img src="../../assets/bg-login-form.png" alt="" class="background">
+    
+    <form action="#" method="post" >
+        <div class="form_left d-flex flex-column justify-content-center align-items-center">
+            <img src="../../assets/moviso-logo1.png" class="" alt="" width="90%">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.Velit aut facilis autem odio 
+                exercitationem Lorem ipsum dolor sit amet consectetur, adipisicing elit consectetur, adipisicing elit</p>
+        </div>
+
+        <div class="form_right p-3 d-flex flex-column justify-content-center align-items-center">
+            <div class="container">
+                <h2 class="text-center text">LOGIN</h2><br>
+            
+                <input type="text" class="form-control" placeholder="Email or phone *" name="emailorphone" >
+            
+                <input type="password" class="form-control" placeholder="Password *" name="Password">
+
+                <a href="#" class="d-flex flex-column align-items-center text-decoration-none ">Forget Password?</a><br>
+
+                <input type="submit" class=" login form-control " value="LOGIN">
+                
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
