2c2b7b1fde94e9295edb0b33f5b9350629ac4402
YUN.CHHILIN <YUN.CHHILIN@PNC.LAN>
2023-01-24 07:50:31 +0700

    [MOS-20] form register seller

---
+++ views/register/view.register.seller.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seller Register</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="../css/bootstrap.min.css"> -->
+</head>
+<body>
+    <div>
+        <img src="../../assets/seller-form.jpg" class="bg" alt="">
+    </div>
+    
+<section class="h-100 ">
+    
+  <div class="container py-5 h-100 ">
+    <div class="row d-flex justify-content-center align-items-center  h-100 ">
+      <div class="col ">
+        <div class="card card-registration my-4 form ">
+          <div class="row g-0">
+            <div class=" form-img col-xl-6 d-none d-xl-block d-flex ter" >               
+                <div class="form-img  d-flex justify-content-center align-items-center">
+                    <img src="../../assets/moviso-logo1.png" alt="" class="p-md-5  d-flex justify-content-center align-items-center"  width="350px" height="" />
+                </div>
+
+                <div>
+                    <h2 class="text-center fs-7 font-weight-bold ">Wellcome </h2>
+                </div>
+
+                <div class="p-md-5">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam eum sit repellendus reiciendis eligendi nisi amet. Laudantium, ex omnis autem hic asperiores ipsa fuga temporibus illo quae minus architecto animi! </p>
+                </div>
+
+                <div class="p-md-5 text-center">
+                    <h2 class="p-md-4 ">
+                        <i class="fa fa-google mr-4" style="font-size:45px;color:dark"></i>
+                        <i class="fa fa-telegram mr-2" style="font-size:45px;color:danger"></i>
+                        <i class="fa fa-twitter m-2" style="font-size:45px;color:primary"></i>
+                    </h2>
+                </div>
+
+            </div>
+            <div class="col-xl-6 ">
+              <div class="card-body p-md-5 ">
+                <h3 class="mb-5 text-center text-uppercase font-weight-bold">registration</h3>
+
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <div class="form-outline">
+                      <input type="text" id="form3Example1m" class="form-control form-control-lg" placeholder="company's name" />
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 mb-4">
+                    <div class="form-outline">
+                      <input type="text" id="form3Example1n" class="form-control form-control-lg" placeholder="phone number" />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input type="email" id="form3Example8" class="form-control form-control-lg" placeholder="enter your email" /><br>
+                  <input type="password" id="form3Example8" class="form-control form-control-lg" placeholder="enter your password"/><br>
+                  <input type="text" id="form3Example8" class="form-control form-control-lg" placeholder="enter your credit card" /><br>
+                  <input type="text" id="form3Example8" class="form-control form-control-lg" placeholder="enter your location" /><br>
+
+                <div class="d-flex justify-content-end p-2">
+                  <button type="button" class="btn btn-back btn-lg  m-2 ">Back</button>
+                  <button type="button" class="btn btn-submit btn-lg ms-2  m-2">Submit</button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</body>
+</html>
