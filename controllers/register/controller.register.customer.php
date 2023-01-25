e5deb117879795e1abd4b8b2d30525aadfde7ccc
DARATH.PUM <DARATH.PUM@PNC.LAN>
2023-01-25 09:24:46 +0700

    [MOS-21] Fix get data from user input and validate form

--- controllers/register/controller.register.customer.php
+++ controllers/register/controller.register.customer.php
@@ -1,15 +1,13 @@
 
 <?php
 require ("../../database/database.php");
+require ('../../model/model.register.php');
 require ("../validate/controller.validate.from.php");
 require("../../views/register/view.register.customer.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ( $userName_valid && $email_valid && $password_valid  && $creditCard_valid  && $date_valid){
-        require ('../../model/model.register.php');
         $userId = createUser($_POST["email"],$_POST["password"]);
-     
-
         createCustomer($_POST["username"],$_POST["creditcard"],$_POST["dateofbirth"], $userId);
 
     }
