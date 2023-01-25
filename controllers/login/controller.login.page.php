f76c62ab7879ed4b8bd10ee337a1819b0db02a84
CHAMRETH.KOSAL <CHAMRETH.KOSAL@pnc.lan>
2023-01-25 20:24:23 +0700

    [MOS-33]MODEL: Get data from database

--- controllers/login/controller.login.page.php
+++ controllers/login/controller.login.page.php
@@ -7,18 +7,15 @@ $users = $statement->fetchAll();
 $isFound=false;
 
 foreach ($users as $user):
-    if (!empty($_POST["password"]) && !empty($_POST["email"])){
-        if(($user["password"] ==$_POST["password"]) && ($user["email"] == $_POST["email"])){
-            $isFound=true;  
-        }
-    }    
+if (!empty($_POST["password"]) && !empty($_POST["email"])){
+    if(($user["password"] ==$_POST["password"]) && ($user["email"] == $_POST["email"])){
+        $isFound=true;  
+    }
+}
 endforeach;
 if($isFound==false){
 require ("../../views/login/view.login.form.php");
 }
 else{
-    require ("../home/home.php");
+require ("../home/home.php"); 
 }
-
-
-
