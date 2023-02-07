dfd84b569bebee163b238452dbfcb39d6e63be37
DARATH.PUM <DARATH.PUM@PNC.LAN>
2023-02-07 20:45:18 +0700

    Fix code view moive

--- model/model.php
+++ model/model.php
@@ -12,15 +12,18 @@ function createCustomer($userId) : bool
 }
 
 
-function createUser(string $userName, string $email, string $password, int $phoneNumber, bool $role = TRUE) : int
+function createUser($userName, $email, $address,$creditCard, $date,  $password, $phoneNumber, bool $role = TRUE) : bool
 {
     global $connection;
-    $statement = $connection->prepare("insert into users (user_name,email,password,phone_number,role) values (:username,:email,:password,:phonenumber,:role)");
+    $statement = $connection->prepare("insert into users (user_name,email,address,credit_card_number,date_of_birth,password,phone_number,role) values (:username,:email,:address,:creditcard,:date,:password,:phonenumber,:role)");
     $statement->execute([
-        'username' => $userName,
+        ':username' => $userName,
         ':email' => $email,
+        ':address' => $address,
+        ':creditcard' => $creditCard,
+        ':date' => $date,
         ':password' => $password,
-        ':phonenumber' => $phoneNumber,
+        ':phonenumber' => $phoneNumber, 
         ':role' => $role,
     ]);
     $statement = $connection->query("SELECT * FROM users");
@@ -28,6 +31,7 @@ function createUser(string $userName, string $email, string $password, int $phon
 
     $number = count($result) - 1;
     $userRole = $result[$number];
+    print_r($userRole['user_id']);
     return $userRole['user_id'];
 }
 
@@ -54,7 +58,7 @@ function getMoives()
     $statement = $connection->prepare("SELECT * FROM movies");
     $statement->execute();
     return $statement->fetchAll();
-    ;
+    
 }
 
 function getSearch($letter)
@@ -84,21 +88,4 @@ function createShow(string $title, int $price, string $genre, string $duration, 
             ':price' => $price,
         ]
     );
-}
-
-function newVenue($name, $address){
-    $statement=$connection->prepare("INSERT INTO venue (name, address) VALUES (:name, :address)");
-    $statement->execute(
-        [
-            ':name' => $name,
-            ':address' => $address,
-        ]
-        );
-    $statement=$connection->prepare("SELECT * FROM venue");
-    $result=$statement->fetcAll();
-
-    $number= count($result)-1;
-    $venueId=$result[$number];
-    return $venueId['venue_id'];
-
 }
