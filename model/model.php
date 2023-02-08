89cf1ec9bad93f49700ab526b140ef5452733e14
nit123456 <nit.noem.@student.passerellesnumeriques.org>
2023-02-08 14:03:03 +0700

    [MOS-83] check again

--- model/model.php
+++ model/model.php
@@ -66,14 +66,16 @@ function getSearch($letter)
     return $state->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function createShow(string $title, int $price, string $genre, string $duration, date $released, string $country, string $production, string $img, string $trailer, string $description)
+function createShow(string $title, int $number_ticket, datetime $date_time, int $price, string $genre, string $duration, date $released, string $country, string $production, string $img, string $trailer, string $description)
 {
     global $connection;
-    $statement=$connection->prepare("INSERT into movies (movie_title, description, genre, duration, released, country, production, trailer, img, price) VALUES (:title, :description, :genre, :duration, :released, :country, :production, :img, :trailer, :pricee)"); 
+    $statement=$connection->prepare("INSERT into movies (movie_title, number_ticket, date_time, description, genre, duration, released, country, production, trailer, img, price) VALUES (:title, :number_ticket, :date_time, :description, :genre, :duration, :released, :country, :production, :img, :trailer, :price)"); 
     $statement->execute(
         [
             ':title' => $title,
             ':description' => $description,
+            ':number_ticket' => $number_ticket,
+            ':date_time'=> $date_time,
             ':genre' => $genre,
             ':duration' => $duration,
             ':released' => $released,
