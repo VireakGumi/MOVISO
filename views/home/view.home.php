e9afa5f63959e31712ae760e76b4991672b0cbf6
VireakGumi <chanvireak906@gmail.com>
2023-02-10 20:29:49 +0700

    create slide show in main

--- views/home/view.home.php
+++ views/home/view.home.php
@@ -3,17 +3,27 @@
 require_once('views/partials/navbar.php');
 ?>
 
-<header class=" masthead text-white text-center pb-5">
-    <img src="../../assets/poster.jpg" class='background' alt="" style=" position: absolute;
-    left: 0px;
-    top: 30px;
-    z-index: -1;
-    -webkit-filter: blur(3px);
-    filter: blur(3px);">
-    <div class="container d-flex align-items-center flex-column">
-        <!-- Masthead Avatar Image-->
+<header class="masthead text-white text-center pb-5" style="height: 450px ;">
+    <div id="slideshow">
+        <div>
+            <img src="../../assets/0x0.jpg">
+        </div>
+        <div>
+            <img src="../../assets/avatar( the blockebuster).png">
+        </div>
+        <div>
+            <img src="../../assets/background_seller_or_customer_register.jpg">
+        </div>
+        <div>
+            <img src="../../assets/poster.jpg">
+        </div>
+    </div>
+    <div class="parallelogram"></div>
+    <div class="parallelogram2"></div>
+    <div class="container d-flex align-items-center flex-column mt-5 heading-text">
+        <!-- Masthead Avatar Image -->
         <img class="masthead-avatar" src="../../assets/moviso-logo1.png" alt="..." width="50%" />
-        <!-- Masthead Heading-->
+        <!-- Masthead Heading -->
         <h1 class="masthead-heading text-uppercase mb-0">Booking Now</h1>
 
         <p class="masthead-subheading font-weight-light mb-0">Action - Romantic - Adventure</p>
@@ -54,6 +64,20 @@ require_once('views/partials/navbar.php');
     </div>
 </section>
 
+<!-- <script>
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 6000);
+
+    dateCheck()
+</script> -->
 
 <?php
 require_once('views/partials/footer.php');
