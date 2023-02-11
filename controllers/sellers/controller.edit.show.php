c2e1c4c3fdc16386e8b1e9b9e9eaf12ff3894928
YUN.CHHILIN <YUN.CHHILIN@pnc.lan>
2023-02-11 13:18:05 +0700

    [MOS-112] controller edit show

---
+++ controllers/sellers/controller.edit.show.php
@@ -0,0 +1,30 @@
+<?php 
+  require "../../database/database.php";
+  require "../../model/model.php";
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $title = $_POST["title"];
+      $price = $_POST["price"];
+      $genre = $_POST["genre"];  
+      $duration = $_POST["duration"];
+      $released = $_POST["released"];
+      $country = $_POST["country"];
+      $dateTime = $_POST["datetime"];
+      $name = $_POST["name"];
+      $production = $_POST["production"];
+      $address = $_POST["address"];
+      $trailer = $_POST["trailer"];
+      $numberTicket = $_POST["numberticket"];
+      $old = $_POST["oldImage"];
+      $img = $_FILES['image']['name'];
+      if($img!="") {
+        move_uploaded_file($_FILES['image']['tmp_name'],'../../assets/'.$img);
+      } else {
+        $img = $old;
+      }
+      $description = $_POST["description"];
+      $id = $_POST["id"];
+        updateMovie($id, $title, $genre, $price, $released, $duration, $numberTicket, $dateTime, $description, $country, $production, $trailer, $img);        
+        updateVenue($id,$name,$address);
+        header("Location:/");
+      
+  }
