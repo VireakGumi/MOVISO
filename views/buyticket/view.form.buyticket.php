0fcbd78a64101f45806fb0f65fceaa16afa023c7
DARATH.PUM <DARATH.PUM@PNC.LAN>
2023-02-13 15:04:33 +0700

    [MOS-116] fix validation form and view form

--- views/buyticket/view.form.buyticket.php
+++ views/buyticket/view.form.buyticket.php
@@ -1,8 +1,8 @@
 <?php require("views/partials/header.php"); ?>
 
-<div class="container-fluid mx-auto">
+<div class="container-fluid mx-auto w-100">
     <img src="../../assets/cinema.png" alt="" class="background">
-        <div class="row d-flex justify-content-center">
+        <form action="#" method="post"class="row d-flex justify-content-center w-100">
             <div class="col-xl-7 col-lg-8 col-md-9 col-sm-11" >
                 <div class="card-ticket border-0">
                     <div class="d-flex justify-content-center mt-2">
@@ -10,36 +10,43 @@
                     </div>
                     <h3 class="mt-2 credit-card">Credit Card Checkout</h3>
                     <div class="row">
-                        <div class="col-sm-7 border-line pb-4 px-5">
+                        <div class="col-sm-7 border-line pb-2 px-5">
                             <div class="form-group">
                                 <p class="text-ticket text-sm mb-0">Name on the card*</p>
                                 <input type="text" name="name" placeholder="Name" class="width-card">
                             </div>
-                            
-                            <div class="form-group d-flex mt-2">
+                            <small class="text-danger">
+
+                                <?=$cardName_error?>
+                              
+                            </small>
+                            <div class="form-group d-flex mt-2 ">
                                 <div id="card-field">
                                     <p class="text-ticket text-sm mb-0">Card Number*</p>
                                     <input type="text" name="card-num" id="card_value" placeholder="0000 0000 0000 0000" width="55" class="width-card" onkeypress="cardCheck()" onclick="normal('card-field')"  maxlength="19">
-                                    <small class="text-danger">
-                                        <?php echo $cardNumber_error; ?>
-                                    </small>
+                                    
                                 </div>
                                 <div class="mt-4">
-                                    <p class="ms-4">/</p>
+                                    <p class="ms-5">/</p>
                                 </div>
                                 <div class="mt-3">
                                     <img class="" src="../../assets/ticketcard.png" width='55'>
                                 </div>
                             </div>
-                            <p class="text-ticket text-sm mb-0 mt-2">Quantity*</p>
-                            <small class="text-danger">
-                                <?php echo $quantityTicket_error; ?>
+                            <small class="text-danger w-100">
+                                        <?=$cardNumber_error?>
                             </small>
+                            <p class="text-ticket text-sm mb-0 mt-2">Quantity*</p>
+                            
                             <div class="form-group d-flex">
                                 <div class="col-md-3 col-lg-3 col-xl-2 d-flex width-card">
                                     <input id="form1"  name="quantity" value="1" type="number" maxlength="3" class="from-control width-card"/>
                                 </div>
                             </div>
+                            <small class="text-danger">
+                               
+                                <?=$quantityTicket_error?>
+                            </small>
                             <button type="submit" class="width-card mt-4 ms-0">Choose sit</button>
                             <div class="form-group mb-0">
                                 <div class="custom-control custom-checkbox custom-control-inline">
@@ -61,6 +68,7 @@
                 </div>
             </div>
         </div>
+ 
 </div>
 <script>
     var cardNum = document.getElementById('card_value');
