8e0a7dea200f9a15c1cbbcd0b165172243ccb799
YUN.CHHILIN <YUN.CHHILIN@PNC.LAN>
2023-02-14 14:21:16 +0700

    update folder structure

--- router.php
+++ router.php
@@ -2,12 +2,12 @@
 
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-// $seller = 'views/erorrs/404.php';
+$seller = 'views/erorrs/404.php';
 
-// if (isset($_COOKIE['Role']) && $_COOKIE['Role'] == 0)
-// {
+if (isset($_COOKIE['Role']) && $_COOKIE['Role'] == 0)
+{
     $seller = 'controllers/sellers/listpage/controller.seller.php';
-// };
+};
 
 $router = [
     "/" => 'controllers/home/controller.home.php',
@@ -21,7 +21,7 @@ $router = [
     "/movie" => 'controllers/sellers/newshow/controller.add.movie.php',
     "/delete" => 'controllers/sellers/listpage/controller.delete.show.php',
     "/update" => 'controllers/sellers/listpage/controller.edit.show.php',
-    "/edit" => 'views/sellers/view.edit.show.php',
+    "/edit" => 'views/sellers/listpage/view.edit.show.php',
     
     
     
