0a7d572741c9ed9546840145cf4b99364ed87b5b
YUN.CHHILIN <YUN.CHHILIN@pnc.lan>
2023-02-14 13:30:20 +0700

    fix code edit in controller

--- controllers/sellers/listpage/controller.edit.show.php
+++ controllers/sellers/listpage/controller.edit.show.php
@@ -1,8 +1,8 @@
 <?php 
 require ("controllers/validate/controller.validate.form.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
-$id = $_POST["id"];
 {
+      $id = $_POST["id"];
       $venueID = $_POST["venueId"];
       $title = $_POST["title"];
       $price = $_POST["price"];
@@ -18,19 +18,17 @@ $id = $_POST["id"];
       $numberTicket = $_POST["numberticket"];
       $old = $_POST["oldImage"];
       $img = $_FILES['image']['name'];
+      $description = $_POST["description"];
       if($img!="") {
         move_uploaded_file($_FILES['image']['tmp_name'],'assets/'.$img);
-      } else {
+      }else {
         $img = $old;
       }
-      $description = $_POST["description"];
-      if( $title_valid && $genre_valid && $duration_valid && $released_valid && $dateTime_valid &&  $numberTicket_valid && $address_valid && $numberTicket_valid && $image_valid && $description_valid && $price_valid &&  $country_valid && $cinemaName_valid && $production_valid && $linkTrailer_valid && $dateTime_valid){
-        updateMovie($id, $title, $genre, $price, $released, $duration, $numberTicket, $dateTime, $description, $country, $production, $trailer, $img);        
-        updateVenue($venueID,$name,$address);
-        header("Location:/");
-        
-      }
-      else{
+      if($title_valid && $genre_valid && $duration_valid && $released_valid && $dateTime_valid &&  $numberTicket_valid && $address_valid && $numberTicket_valid && $image_valid && $description_valid && $price_valid &&  $country_valid && $cinemaName_valid && $production_valid && $linkTrailer_valid && $dateTime_valid){
+          updateMovie($id, $title, $genre, $price, $released, $duration, $numberTicket, $dateTime, $description, $country, $production, $trailer, $img);        
+          updateVenue($venueID,$name,$address);
+          header("Location:/");
+      }else{
           $path = "/edit?id=".$id;
           header("Location:$path");
         }
