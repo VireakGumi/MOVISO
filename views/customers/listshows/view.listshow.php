fb628b3fd109b7b0ef2a18bed54b4d73d544a8e4
YUN.CHHILIN <YUN.CHHILIN@PNC.LAN>
2023-02-16 20:08:37 +0700

    fix code

--- views/customers/listshows/view.listshow.php
+++ views/customers/listshows/view.listshow.php
@@ -1,11 +1,10 @@
-    <?php
-require_once('views/partials/navbar.php');
-$row = 16;
+<?php
+    require_once('views/partials/navbar.php');
+    $row = 16;
 ?>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
 .navbar a {
     text-decoration: none;
 }
@@ -29,70 +28,67 @@ $row = 16;
 .nav>li>a:hover {
     color: white !important;
 }
-    </style>
+</style>
 
 
-    <div class="list">
-        <div class="d-flex ms-5">
-            <h3 class="text-white">CATEGROY</h3>
-            <div class="d-flex flex-column w-100" id="search-blog">
-                <div class="d-flex">
-                    <form action="" method="get" id="search" class="w-100">
-                        <input type="search" class="form-control" placeholder="Search" id="fname" name="fname"
-                            onkeyup="showHint(this.value)">
-                        <label for="fname" class="cursor-pointer text-white"><i
-                                class="material-icons">search</i></label>
-                    </form>
-                </div>
-                <div id="product" class="text-white d-flex flex-column mx-3">
-                    
-                </div>
+<div class="list">
+    <div class="d-flex ms-5">
+        <h3 class="text-white">CATEGROY</h3>
+        <div class="d-flex flex-column w-100" id="search-blog">
+            <div class="d-flex">
+                <form action="" method="get" id="search" class="w-100">
+                    <input type="search" class="form-control" placeholder="Search" id="fname" name="fname"
+                        onkeyup="showHint(this.value)">
+                    <label for="fname" class="cursor-pointer text-white"><i class="material-icons">search</i></label>
+                </form>
+            </div>
+            <div id="product" class="text-white d-flex flex-column mx-3">
+
             </div>
         </div>
-        <ul class="section-of-movies  nav nav-tabs d-flex mt-5 mx-4 border-none">
-            <li class="active  m-3"><a data-toggle="tab" href="#allMovies" class="fw-bold text-white">ALL MOVIES</a>
-            </li>
-            <li class=" m-3"><a data-toggle="tab" href="#action" class="fw-bold text-white">ACTION</a></li>
-            <li class=" m-3"><a data-toggle="tab" href="#comedy" class="fw-bold text-white">COMEDY</a></li>
-            <li class=" m-3"><a data-toggle="tab" href="#crime" class="fw-bold text-white">CRIME</a></li>
-        </ul>
+    </div>
+    <ul class="section-of-movies  nav nav-tabs d-flex mt-5 mx-4 border-none">
+        <li class="active  m-3"><a data-toggle="tab" href="#allMovies" class="fw-bold text-white">ALL MOVIES</a>
+        </li>
+        <li class=" m-3"><a data-toggle="tab" href="#action" class="fw-bold text-white">ACTION</a></li>
+        <li class=" m-3"><a data-toggle="tab" href="#comedy" class="fw-bold text-white">COMEDY</a></li>
+        <li class=" m-3"><a data-toggle="tab" href="#crime" class="fw-bold text-white">CRIME</a></li>
+    </ul>
 
-        <div class="tab-content">
-            <div id="allMovies" class="tab-pane fade in active">
-                <div class="card-container mt-5">
-                    <?php
+    <div class="tab-content">
+        <div id="allMovies" class="tab-pane fade in active">
+            <div class="card-container mt-5">
+                <?php
                     $movies = getMoives();
-                    // print_r($movies);
                     require('views/createCard.php');
                     ?>
-
-                </div>
             </div>
-            <div id="action" class="tab-pane fade">
-                <div class="card-container mt-5">
-                    <?php
+        </div>
+        <div id="action" class="tab-pane fade">
+            <div class="card-container mt-5">
+                <?php
                         $movies = sectionMovies('action');
                         require('views/createCard.php');
                     ?>
-                </div>
             </div>
-            <div id="comedy" class="tab-pane fade">
-                <div class="card-container mt-5">
-                    <?php
+        </div>
+        <div id="comedy" class="tab-pane fade">
+            <div class="card-container mt-5">
+                <?php
                         $movies = sectionMovies('comedy');
                         require('views/createCard.php');
                     ?>
-                </div>
             </div>
-            <div id="crime" class="tab-pane fade">
-                <div class="card-container mt-5">
-                    <?php
+        </div>
+        <div id="crime" class="tab-pane fade">
+            <div class="card-container mt-5">
+                <?php
                         $movies = sectionMovies('crime');
                         require('views/createCard.php');
                     ?>
-                </div>
             </div>
         </div>
-
-        <?php require_once('views/partials/footer.php');?>
     </div>
+
+    <?php require_once('views/partials/footer.php');?>
+</div>
